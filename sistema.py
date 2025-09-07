3db434854fe95656c2003e3b3dd7136ac3cc76c8
Iran_Virgilio <152872691+IranVirgilio@users.noreply.github.com>
2025-09-07 17:46:43 -0300

    Add files via upload

--- sistema.py
+++ sistema.py
@@ -12,66 +12,63 @@ class Sistema:
         self.__livros.append(novo_livro)
     
     def listar_usuario(self):
+        if not self.__usuarios:
+            print("Nenhum usuário cadastrado.")
+            return
         for i in self.__usuarios:
             i.impressao()
 
     def listar_livros(self):
+        if not self.__livros:
+            print("Nenhum livro cadastrado.")
+            return
         for i in self.__livros:
             i.impressao()
     
     def emprestimo(self, nome_livro, matricula):
-        #buscar livro
         livro_encontrado = None
-        
         for livro in self.__livros:
             if (livro.get_nome()).upper() == nome_livro.upper():
                 livro_encontrado = livro
                 break
         
         if livro_encontrado is None:
-            raise LivroNaoEncontradoError("Livro não encontrado")
+            raise LivroNaoEncontradoError(nome_livro)
             
-        
         if livro_encontrado.get_quantidade_estocada() == 0:
-            raise EstoqueInsuficienteError("Não possui estoque desse livro")
+            raise EstoqueInsuficienteError(nome_livro)
         
-        #busca usuario
         usuario_encontrado = None
-
         for usuario in self.__usuarios:
             if usuario.get_matricula() == matricula:
                 usuario_encontrado = usuario
                 break
         
         if usuario_encontrado is None:
-            raise MatriculaNaoLocalizadaError("Matricula não localizada")
+            raise MatriculaNaoLocalizadaError(matricula)
         
-        #chegou aqui ta tudo ok
         usuario_encontrado.set_livros_alugados(usuario_encontrado.get_livros_alugados() + 1)
         livro_encontrado.set_quantidade_estocada(livro_encontrado.get_quantidade_estocada() - 1)
-        print(f"Empréstimo realizado: {livro_encontrado.get_nome()} -> {usuario_encontrado.get_nome()}.")
+        print(f"\n>>> Empréstimo realizado: '{livro_encontrado.get_nome()}' -> {usuario_encontrado.get_nome()}.")
         
-
     def devolucao(self, nome_livro, matricula):
         livro_encontrado = None
-        
         for livro in self.__livros:
             if (livro.get_nome()).upper() == nome_livro.upper():
                 livro_encontrado = livro
                 break
         if livro_encontrado is None:
-            raise LivroNaoEncontradoError("Livro não foi localizado")
+            raise LivroNaoEncontradoError(nome_livro)
         
         usuario_encontrado = None
-
         for usuario in self.__usuarios:
             if usuario.get_matricula() == matricula:
                 usuario_encontrado = usuario
                 break
         
         if usuario_encontrado is None:
-            raise MatriculaNaoLocalizadaError("Matricula não localizada")
+            raise MatriculaNaoLocalizadaError(matricula)
 
         usuario_encontrado.set_livros_alugados(usuario_encontrado.get_livros_alugados() - 1)
         livro_encontrado.set_quantidade_estocada(livro_encontrado.get_quantidade_estocada() + 1)
-        print(f"Livro {livro_encontrado.get_nome()} devolvido com sucesso.")
+        print(f"\n>>> Livro '{livro_encontrado.get_nome()}' devolvido com sucesso.")
