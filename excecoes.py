6279b126d35624814602ea5b7010cb12444ea698
Iran_Virgilio <152872691+IranVirgilio@users.noreply.github.com>
2025-09-07 17:45:21 -0300

    Add files via upload

--- excecoes.py
+++ excecoes.py
@@ -1,19 +1,33 @@
-class ValorVazioError(Exception):
-    """Exceção para quando um campo está vazio."""
+class ErroSistema(Exception):
+    """ Classe base para todas as exceções do sistema, captura todos os erros da class
+    de forma genérica"""
     pass
 
-class ValorInvalidoError(Exception):
+class ValorVazioError(ErroSistema):
+    """Exceção para quando um campo está vazio."""
+    def __init__(self, campo):
+        self.mensagem = f"O campo '{campo}' não pode ser vazio!, Tente novamente!"
+        super().__init__(self.mensagem)
+    
+
+class ValorInvalidoError(ErroSistema):
     """Exceção para quando um valor é inválido."""
     pass
 
-class LivroNaoEncontradoError(Exception):
-    """Exceção para quando um livro não é encontrado."""
-    pass
+class LivroNaoEncontradoError(ErroSistema):
+    """Exceção para quando um livro não é encontrado no sistema."""
+    def __init__(self, identificador_livro):
+        self.mensagem = f"O Livro com o identificador '{identificador_livro}' não foi encontrado!. Verifique se esta correto e tente novamente!"
+        super().__init__(self.mensagem)
 
-class MatriculaNaoLocalizadaError(Exception):
+class MatriculaNaoLocalizadaError(ErroSistema):
     """Exceção para quando a matrícula do usuário não é encontrada."""
-    pass
+    def __init__(self, matricula):
+        self.mensagem = f"A matricula digitada: '{matricula}' não foi localizada no sistema. verifique se esta correta!"
+        super().__init__(self.mensagem)
 
-class EstoqueInsuficienteError(Exception):
+class EstoqueInsuficienteError(ErroSistema):
     """Exceção para quando não há estoque disponível."""
-    pass
+    def __init__(self, nome_livro):
+        self.mensagem = f"Não há exemplares do livro {nome_livro} disponíveis em estoque. Tente outro dia!"
+        super().__init__(self, nome_livro)
